c3fb19f42a2d7c448b959037a805dde05e9ab614
Silvino Vieira de Vasconcelos Neto <silvv@br.ibm.com>
2018-02-05 20:41:07 -0300

    Removing Curry, Quick and Nimble dependencies, and using regular XCTest instead

--- MaximoRESTSDK/MaximoRESTSDKTests/MaximoRESTSDKTests.swift
+++ MaximoRESTSDK/MaximoRESTSDKTests/MaximoRESTSDKTests.swift
@@ -6,48 +6,58 @@
 //  Copyright © 2018 IBM. All rights reserved.
 //
 
-import Quick
-import Nimble
+import XCTest
 import MaximoRESTSDK
 
-class MaximoRESTSDKTests: QuickSpec {
+class MaximoRESTSDKTests: XCTestCase {
     
-    override func spec() {
-        describe("MaximoRESTSDK") {
-            it("is a framework to communicate with Maximo") {
-                var options = Options().user(user: "wilson").password(password: "wilson").auth(authMode: "maxauth")
-                options = options.host(host: "9.85.169.134").port(port: 7001).lean(lean: true)
-                let connector = MaximoConnector(options: options)
-                do {
-                    try connector.connect()
-                    print("Login Success!")
-                    
-                    let workOrderSet = connector.resourceSet(osName: "mxwo")
-                    _ = workOrderSet.pageSize(pageSize: 1)
-                    _ = workOrderSet._where(whereClause: "spi:wonum=\"1002\" and spi:istask=0")
-                    _ = workOrderSet.paging(type: true)
-                    _ = try workOrderSet.fetch()
-                    let resource = try workOrderSet.member(index: 0)
-                    var workOrder = try resource!.toJSON()
-
-                    print("Fetching a Work Order Success!")
-                    print("Work Order JSON: ")
-                    print(workOrder)
-
-                    print("Updating a Work Order")
-                    let uri = connector.getCurrentURI() + "/os/mxwo/" + Util.stringValue(value: workOrder["workorderid"]!)
-                    workOrder["description"] = "Maximo iOS Integration Test"
-                    workOrder["wopriority"] = 1
-                    workOrder["estdur"] = 25.0
-                    _ = try connector.update(uri: uri, jo: workOrder, properties: nil)
-
-                    print("Logging out!")
-                    try connector.disconnect()
-                } catch {
-                    print("Maximo connection failure.")
-                }
-            }
+    static var connector : MaximoConnector?
+    static var workOrder : [String: Any]?
+    
+    override class func setUp() {
+        super.setUp()
+        var options = Options().user(user: "wilson").password(password: "wilson").auth(authMode: "maxauth")
+        options = options.host(host: "9.85.129.75").port(port: 7001).lean(lean: true)
+        connector = MaximoConnector(options: options)
+        do {
+            try connector!.connect()
+            print("Login Success!")
+        } catch {
+            print("Error while logging in")
         }
     }
     
+    override class func tearDown() {
+        do {
+            print("Logging out!")
+            try connector!.disconnect()
+        } catch {
+            print("Error while logging out")
+        }
+        super.tearDown()
+    }
+
+    func testLoadWorkOrder() throws {
+        let workOrderSet = MaximoRESTSDKTests.connector!.resourceSet(osName: "mxwo")
+        _ = workOrderSet.pageSize(pageSize: 1)
+        _ = workOrderSet._where(whereClause: "spi:wonum=\"1002\" and spi:istask=0")
+        _ = workOrderSet.paging(type: true)
+        _ = try workOrderSet.fetch()
+        let resource = try workOrderSet.member(index: 0)
+        MaximoRESTSDKTests.workOrder = try resource!.toJSON()
+        
+        print("Fetching a Work Order Success!")
+        print("Work Order JSON: ")
+        print(MaximoRESTSDKTests.workOrder!)
+    }
+
+    func testUpdateWorkOrder() throws {
+        print("Updating a Work Order")
+        let uri = MaximoRESTSDKTests.connector!.getCurrentURI() + "/os/mxwo/" +
+            Util.stringValue(value: MaximoRESTSDKTests.workOrder!["workorderid"]!)
+        MaximoRESTSDKTests.workOrder!["description"] = "Maximo iOS Integration Test"
+        MaximoRESTSDKTests.workOrder!["wopriority"] = 1
+        MaximoRESTSDKTests.workOrder!["estdur"] = 25.0
+        _ = try MaximoRESTSDKTests.connector!.update(uri: uri, jo: MaximoRESTSDKTests.workOrder!, properties: nil)
+    }
 }
