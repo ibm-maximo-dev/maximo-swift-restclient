5a18f456a58558f08e73bf9fbf3790d225195abe
rbrossi <rbrossi@br.ibm.com>
2018-07-26 11:45:14 -0300

    Comments for SavedQuery.swift file

--- MaximoRESTSDK/MaximoRESTSDK/SavedQuery.swift
+++ MaximoRESTSDK/MaximoRESTSDK/SavedQuery.swift
@@ -8,36 +8,62 @@
 
 import Foundation
 
+/// Object to represent the Saved query.
 public class SavedQuery {
     
+    /// Query name
     var name : String?
+    /// Query of Any to represent the query information
     var map : [String: Any]
 
+    /// Init the SavedQuery object.
     public init() {
         name = String()
         map = [:]
     }
 
+    /// Init the SavedQuery object.
+    ///
+    /// - Parameters:
+    ///   - name: Query name.
+    ///   - map: map with query commands.
     public init(name: String, map: [String: Any]) {
         self.name = name
         self.map = map
     }
     
+    /// Set query name.
+    ///
+    /// - Parameter name: String within query name.
+    /// - Returns: Query name updated.
     public func name(name: String) -> SavedQuery{
         self.name = name;
         return self;
     }
 
+    /// Query params.
+    ///
+    /// - Parameter params: Any object containing the query params.
+    /// - Returns: updated map within query parameters.
     public func params(params: [String: Any]) -> SavedQuery {
         self.map = params
         return self
     }
 
+    /// Add a new parameter to the query's parameter map.
+    ///
+    /// - Parameters:
+    ///   - key: Key for the map
+    ///   - value: parameter's value.
+    /// - Returns: map updated, including the new parameter.
     public func addParam(key: String, value: Any) -> SavedQuery {
         map[key] = value
         return self
     }
     
+    /// Save query function.
+    ///
+    /// - Returns: Saved query.
     public func savedQueryClause() -> String {
         var strBuilder = String()
         strBuilder.append(self.name!)
